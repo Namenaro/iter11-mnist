14f71189bba885218526e74a0b86eac36acd1800
neuro <nanenaro@gmail.com>
2021-07-06 16:57:56 +0300

    Initial commit

---
+++ hypothesis.py
@@ -0,0 +1,57 @@
+
+from sensors import *
+
+import numpy as np
+import math
+import operator
+
+class Hypothesis:
+    def __init__(self, sensor_field_radius, etalon_mean, dx, dy, u_radius):
+        self.sensor_field_radius = sensor_field_radius
+        self.etalon_mean = etalon_mean
+
+        self.dx = dx
+        self.dy = dy
+        self.u_radius = u_radius
+
+    def _apply_to_point_no_u(self, pic, x, y):
+        sensory_array = get_sensory_array(pic, x, y, self.sensor_field_radius)
+        current_mean = np.mean(sensory_array)
+        deviation_from_etalon = math.pow((self.etalon_mean - current_mean),2)
+        return deviation_from_etalon
+
+    def apply_to_point(self, pic, x, y):
+        expected_x = x + self.dx
+        expected_y = y + self.dy
+
+        X, Y = get_coords_less_or_eq_raduis(expected_x, expected_y, self.u_radius)
+        temporary_hypotheses = {}
+        for i in range(len(X)):
+            deviation_from_etalon = self._apply_to_point_no_u(pic, X[i], Y[i])
+            ddx = X[i] - expected_x
+            ddy = Y[i] - expected_y
+
+            temporary_hypotheses[(ddx, ddy)] = deviation_from_etalon
+
+        deviation_from_etalon, ddx, ddy = find_best_hypothesys(temporary_hypotheses)
+        return deviation_from_etalon, ddx, ddy
+
+def find_best_hypothesys(dict_hypotheses): #ищем ту что с самым маленьким значением
+    sorted_hypos = sorted(dict_hypotheses.items(), key=operator.itemgetter(1),reverse=False)
+    best_hypo = sorted_hypos[0]
+    dxdy= best_hypo[0]
+    val = best_hypo[1]
+    return val, dxdy[0], dxdy[1]
+
+def get_field_of_hypo_on_pic(hypo, pic):
+        ymax = pic.shape[0]
+        xmax = pic.shape[1]
+
+        res = np.zeros((ymax, xmax))
+        for centery in range(0, ymax):
+            for centerx in range(0, xmax):
+                val = hypo.apply_to_point(pic, centerx, centery)
+                res[centery, centerx] = val
+        return res
+
+
---
+++ plotter.py
@@ -0,0 +1,43 @@
+import operator
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def plot_points_on_pic(pic, X,Y, colors=None):
+    if colors is None:
+        colors = 'green'
+    fig, ax = plt.subplots()
+    plt.imshow(pic, cmap='gray_r')
+    plt.scatter(X[0], Y[0], s=100, c='red', marker='o', alpha=0.4)
+    plt.scatter(X[1:], Y[1:], s=100, c=colors, marker='o', alpha=0.4)
+    return fig
+
+def plot_several_graphs(graphs, names):
+    fig, axs = plt.subplots(len(graphs))
+    for i in range(len(graphs)):
+        axs[i].plot(graphs[i])
+        axs[i].set_title(names[i])
+    return fig
+
+def plot_several_lines_pics_with_one_colorbar(pics_series):
+    rows = len(pics_series)
+    num_pics_in_seria = len(pics_series[0])
+    fig, axs = plt.subplots(rows, num_pics_in_seria)
+    MIN, MAX = np.array(pics_series).min(), np.array(pics_series).max()
+    for row in range(rows):
+        for col in range(num_pics_in_seria):
+            im = axs[row, col].imshow(pics_series[row][col], cmap='Blues', vmin=MIN, vmax=MAX)
+
+    fig.colorbar(im, ax=axs.ravel().tolist())
+    return fig
+
+def plot_several_pics_with_one_colorbar(pics):
+    num_pics_in_seria = len(pics)
+    fig, axs = plt.subplots(num_pics_in_seria)
+    MIN, MAX = np.array(pics).min(), np.array(pics).max()
+
+    for i in range(num_pics_in_seria):
+        im = axs[i].imshow(pics[i], cmap='Blues', vmin=MIN, vmax=MAX)
+
+    fig.colorbar(im, ax=axs.ravel().tolist())
+    return fig
